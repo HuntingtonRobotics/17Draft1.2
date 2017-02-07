b4f0f23c86ffcf96bd0fae72202a3798f3102f22
mgen2000 <mattgennarelli@optonline.net>
2017-02-07 16:35:24 -0500

    Added the Gear and Shooter Camera

--- src/org/usfirst/frc/team5016/robot/subsystems/Camera.java
+++ src/org/usfirst/frc/team5016/robot/subsystems/Camera.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team5016.robot.subsystems;
 
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -7,12 +8,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Camera extends Subsystem {
+	UsbCamera gearCam; // USB 0, Server 1181
+	UsbCamera shooterCam; // USB 1, Server 1182
+	
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
-    	CameraServer.getInstance().startAutomaticCapture();
+    	gearCam = CameraServer.getInstance().startAutomaticCapture();
+    	shooterCam = CameraServer.getInstance().startAutomaticCapture();
     }
 }
 
