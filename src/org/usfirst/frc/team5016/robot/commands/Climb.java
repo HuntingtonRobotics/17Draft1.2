97a31001be3a48050074790c7f2bb88ac895cc02
mgen2000 <mattgennarelli@optonline.net>
2017-02-10 15:30:10 -0500

    added Robot.climber

--- src/org/usfirst/frc/team5016/robot/commands/Climb.java
+++ src/org/usfirst/frc/team5016/robot/commands/Climb.java
@@ -3,8 +3,6 @@ package org.usfirst.frc.team5016.robot.commands;
 import org.usfirst.frc.team5016.robot.Robot;
 import org.usfirst.frc.team5016.robot.RobotMap;
 
-import com.ctre.CANTalon;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -13,18 +11,23 @@ import edu.wpi.first.wpilibj.command.Command;
 public class Climb extends Command {
 
     public Climb() {
+    	
+    	requires(Robot.climber);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
-    CANTalon climbMotor;
+    
     protected void initialize() {
-    	Robot.climber.climbMotor.set(RobotMap.climbMotorSpeed);
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	Robot.climber.climbMotor.set(RobotMap.climbMotorSpeed);
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -34,7 +37,7 @@ public class Climb extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	climbMotor.set(0);//Turn off climb motor
+    	Robot.climber.climbMotor.set(0);//Turn off climb motor
     }
 
     // Called when another command which requires one or more of the same
