174a4203b7292d3cb588891082b6e977637e594b
mgen2000 <mattgennarelli@optonline.net>
2017-02-10 15:20:01 -0500

    Added speed set in RobotMap

--- src/org/usfirst/frc/team5016/robot/commands/MecanumDrive.java
+++ src/org/usfirst/frc/team5016/robot/commands/MecanumDrive.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5016.robot.commands;
 
 import org.usfirst.frc.team5016.robot.Robot;
+import org.usfirst.frc.team5016.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -24,7 +25,9 @@ public class MecanumDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.chassis.mecanumDrive_Cartesian(Robot.oi.driveGamePad.getRawAxis(0), Robot.oi.driveGamePad.getRawAxis(1), Robot.oi.driveGamePad.getRawAxis(4), Robot.gyroscope.angle);
+    	
+    	Robot.driveTrain.chassis.mecanumDrive_Cartesian(Robot.oi.driveGamePad.getRawAxis(0) * RobotMap.driveSpeed, Robot.oi.driveGamePad.getRawAxis(1) * RobotMap.driveSpeed, Robot.oi.driveGamePad.getRawAxis(4) * RobotMap.driveSpeed, Robot.gyroscope.angle);
+    
     }
 
     // Make this return true when this Command no longer needs to run execute()
